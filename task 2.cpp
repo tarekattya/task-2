9e57c60718b397143880a1eabd2698496fe6bd83
tarekattya <87292867+tarekattya@users.noreply.github.com>
2021-11-05 11:10:21 +0200

    Update task 2.cpp

--- task 2.cpp
+++ task 2.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
-int main()  { 
+int main()  {
+//we declaration some variable  
 int sec , hours , min , sec1;
+//take a input from user and print it.
 printf("input secends:\t");
-scanf("%d",&sec);
-hours=(sec/3600);
 
+scanf("%d",&sec);
+//caculating the number of hours
+hours=(sec/3600);
+//caculating the number of minutes
 min=(sec-(3600*hours))/60;
+//caculating the number of scends
 
 sec1= (sec-(3600*hours)-(min*60));
+
+//print the output 
 
 printf("Hours - %d\n:Minute - %d\n:Secends - %d\n ",hours,min,sec1);
 return 0;
